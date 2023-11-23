9d4ad6d5204001214559c7402066ba4aff2be702
SnubStumpy2007 <joshua192007@gmail.com>
2023-11-22 19:34:38 -0500

    update

--- src/Checkbox.jsx
+++ src/Checkbox.jsx
@@ -1,10 +1,18 @@
-import React from "react";
+import React, { useState} from "react";
 
 export const Checkbox = ({grocery}) => {
+    const [isChecked, setIsChecked] = useState(false)
+    const [isVisible, setIsVisible] = useState(true)
+    const [isCrossed, setIsCrossed] = useState(false)
+    
+    function handleChange() {
+        setIsChecked(true)
+    }
+
     return(
         <div className="item">
             <div className="input-container">
-                
+                <input type="checkbox"  checked={isChecked} onChange={handleChange}/>
             </div>
             <p>
             {grocery}
